dbaaff8fbbc51e8ad5674d05d73cbd95742b9a4c
rakinuddin <rakinuddin97@gmail.com>
2019-03-05 13:13:41 -0500

    made resolver merging more robust

--- server/resolvers/index.js
+++ server/resolvers/index.js
@@ -1,13 +1,17 @@
+const _ = require('lodash');
 const tiles = require('./tiles');
 
-module.exports = {
-  Orientation: {
-    UP: 0,
-    RIGHT: 1,
-    DOWN: 2,
-    LEFT: 3,
+_.merge(module.exports,
+  {
+    Orientation: {
+      UP: 0,
+      RIGHT: 1,
+      DOWN: 2,
+      LEFT: 3,
+    },
   },
-  Query: {
+  {
+    Query: {
+    },
   },
-  ...tiles,
-};
+  tiles);
