96474309388135d9bb16ecd92b304b4ef9608588
zhangke <mstr.zhang@gmail.com>
2019-03-08 00:56:27 -0500

    add pixi and minor style changes

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -4,7 +4,7 @@ import './Board.css';
 class Board extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="container-fluid">
         this is great
       </div>
     );
