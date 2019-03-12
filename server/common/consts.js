11b7c70a2a4fc259a9b6d9a6248e385f08a30a77
rakinuddin <rakinuddin97@gmail.com>
2019-03-12 19:09:51 -0400

    base tile config made

---
+++ server/common/consts.js
@@ -0,0 +1,25 @@
+const MAZETILE1_TILE_NEIGHBOUR_CONFIG = [
+  { type: '', neighbours: [null, 0, 0, 1] },
+  { type: '', neighbours: [null, 0, 0, 1] },
+  { type: '', neighbours: [null, 0, 0, 1] },
+  { type: '', neighbours: [null, 0, 0, 1] },
+  { type: '', neighbours: [null, 0, 0, 1] },
+  { type: '', neighbours: [null, 0, 0, 1] },
+  { type: '', neighbours: [null, 0, 0, 1] },
+  { type: '', neighbours: [null, 0, 0, 1] },
+  { type: '', neighbours: [null, 0, 0, 1] },
+  { type: '', neighbours: [null, 0, 0, 1] },
+  { type: '', neighbours: [null, 0, 0, 1] },
+  { type: '', neighbours: [null, 0, 0, 1] },
+  { type: '', neighbours: [null, 0, 0, 1] },
+  { type: '', neighbours: [null, 0, 0, 1] },
+  { type: '', neighbours: [null, 0, 0, 1] },
+  { type: '', neighbours: [null, 0, 0, 1] },
+];
+
+const MAZETILE_TILE_CONFIGS = [
+  MAZETILE1_TILE_NEIGHBOUR_CONFIG,
+
+];
+
+module.exports = { MAZETILE_TILE_CONFIGS };
