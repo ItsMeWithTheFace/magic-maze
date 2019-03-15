4c116f30c5e2e1d64f028dffad0be7a3127f1256
zhangke <mstr.zhang@gmail.com>
2019-03-15 16:14:13 -0400

    apollo setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
 import Board from './components/Board';
 import Homepage from './components/Homepage';
 
+const client = new ApolloClient({
+  uri: process.env.API_DEV,
+});
+
 const App = () => (
-  <div>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/board" component={Board} />
-      </Switch>
-    </BrowserRouter>
-  </div>
+  <ApolloProvider client={client}>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/board" component={Board} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  </ApolloProvider>
 );
 
 export default App;
