e0e3cfdeaec2fdc0436b4d998a268b95cd7322a3
rakinuddin <rakinuddin97@gmail.com>
2019-03-14 22:50:33 -0400

    fixed initial coordinates for start tile

--- server/common/consts.js
+++ server/common/consts.js
@@ -22,20 +22,20 @@ const MAZETILE_0_TILE_NEIGHBOUR_CONFIG = [
   { type: SEARCH_TYPE, neighbours: [null, 1, 6, 3], colour: ORANGE, searched: false, coordinates: { x: 2, y: 0 } },
   { type: VORTEX_TYPE, neighbours: [-1, 2, -1, -1], colour: PURPLE, coordinates: { x: 3, y: 0 } },
 
-  { type: SEARCH_TYPE, neighbours: [0, null, 8, 5], colour: PURPLE, searched: false, coordinates: { x: 0, y: -1 } },
-  { type: NORMAL_TYPE, neighbours: [1, 4, 9, 6], coordinates: { x: 1, y: -1 } },
-  { type: NORMAL_TYPE, neighbours: [2, 5, 10, 7], coordinates: { x: 2, y: -1 } },
-  { type: VORTEX_TYPE, neighbours: [-1, 6, -1, -1], colour: YELLOW, coordinates: { x: 3, y: -1 } },
+  { type: SEARCH_TYPE, neighbours: [0, null, 8, 5], colour: PURPLE, searched: false, coordinates: { x: 0, y: 1 } },
+  { type: NORMAL_TYPE, neighbours: [1, 4, 9, 6], coordinates: { x: 1, y: 1 } },
+  { type: NORMAL_TYPE, neighbours: [2, 5, 10, 7], coordinates: { x: 2, y: 1 } },
+  { type: VORTEX_TYPE, neighbours: [-1, 6, -1, -1], colour: YELLOW, coordinates: { x: 3, y: 1 } },
 
-  { type: VORTEX_TYPE, neighbours: [-1, -1, -1, 9], colour: ORANGE, coordinates: { x: 0, y: -2 } },
-  { type: NORMAL_TYPE, neighbours: [5, 8, 13, 10], coordinates: { x: 1, y: -2 } },
-  { type: NORMAL_TYPE, neighbours: [6, 9, 14, -1], coordinates: { x: 2, y: -2 } },
-  { type: SEARCH_TYPE, neighbours: [-1, -1, 15, null], colour: GREEN, searched: false, coordinates: { x: 3, y: -2 } },
+  { type: VORTEX_TYPE, neighbours: [-1, -1, -1, 9], colour: ORANGE, coordinates: { x: 0, y: 2 } },
+  { type: NORMAL_TYPE, neighbours: [5, 8, 13, 10], coordinates: { x: 1, y: 2 } },
+  { type: NORMAL_TYPE, neighbours: [6, 9, 14, -1], coordinates: { x: 2, y: 2 } },
+  { type: SEARCH_TYPE, neighbours: [-1, -1, 15, null], colour: GREEN, searched: false, coordinates: { x: 3, y: 2 } },
 
-  { type: VORTEX_TYPE, neighbours: [-1, -1, -1, 13], colour: GREEN, coordinates: { x: 0, y: -3 } },
-  { type: SEARCH_TYPE, neighbours: [9, 12, null, 14], colour: YELLOW, searched: false, coordinates: { x: 1, y: -3 } },
-  { type: NORMAL_TYPE, neighbours: [10, 13, -1, 15], coordinates: { x: 2, y: -3 } },
-  { type: NORMAL_TYPE, neighbours: [11, 14, -1, -1], coordinates: { x: 3, y: -3 } },
+  { type: VORTEX_TYPE, neighbours: [-1, -1, -1, 13], colour: GREEN, coordinates: { x: 0, y: 3 } },
+  { type: SEARCH_TYPE, neighbours: [9, 12, null, 14], colour: YELLOW, searched: false, coordinates: { x: 1, y: 3 } },
+  { type: NORMAL_TYPE, neighbours: [10, 13, -1, 15], coordinates: { x: 2, y: 3 } },
+  { type: NORMAL_TYPE, neighbours: [11, 14, -1, -1], coordinates: { x: 3, y: 3 } },
 ];
 
 const MAZETILE_1_TILE_NEIGHBOUR_CONFIG = [
