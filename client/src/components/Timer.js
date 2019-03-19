4d424161ef1cd3e6555ad09884605712a32e8a81
zhangke <mstr.zhang@gmail.com>
2019-03-19 17:27:51 -0400

    documentation

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 const now = new Date();
+// TODO: pull this value from props
 const timer = new Date(now.getTime() + 1 * 60000);
 const delta = new Date(timer.getTime() - now.getTime());
 
@@ -18,16 +19,20 @@ class Timer extends Component {
 
   tick() {
     const { time } = this.state;
+    // convert seconds to min:sec
     const minutes = Math.floor(time / 60);
     const seconds = time - minutes * 60;
 
     if (time >= 0) {
       this.setState({
+        // handle 0 rollover
         time: time !== 0 ? time - 1 : 0,
         min: minutes,
+        // seconds formatting
         sec: seconds >= 10 ? seconds : `0${seconds}`,
       });
     } else {
+      // kill the timer
       clearInterval(this.timer);
     }
   }
