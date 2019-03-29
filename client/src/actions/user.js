be31fdf57c3bed2219b8e34763fb4206d3c278b7
rakinuddin <rakinuddin97@gmail.com>
2019-03-29 16:38:38 -0400

    updated user action and reducer

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -1,4 +1,6 @@
-import { LOGIN_USER, LOGOUT_USER } from '../common/consts';
+import { SIGNUP_USER, LOGIN_USER, LOGOUT_USER } from '../common/consts';
 
-export const loginUser = uid => ({ type: LOGIN_USER, uid });
+export const signupUser = uid => ({ type: SIGNUP_USER, payload: uid });
+
+export const loginUser = uid => ({ type: LOGIN_USER, payload: uid });
 export const logoutUser = () => ({ type: LOGOUT_USER });
--- client/src/reducers/user.js
+++ client/src/reducers/user.js
@@ -1,16 +1,23 @@
 /* eslint-disable no-param-reassign */
-import { LOGIN_USER, LOGOUT_USER } from '../common/consts';
+import { LOGIN_USER, LOGOUT_USER, SIGNUP_USER } from '../common/consts';
 
 const initialState = {
   uid: null,
+  email: null,
+  username: null,
 };
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SIGNUP_USER:
+      return {
+        ...state,
+        uid: action.payload.uid,
+      };
     case LOGIN_USER:
       return {
         ...state,
-        uid: action.uid,
+        uid: action.payload.uid,
       };
     case LOGOUT_USER:
       return {
