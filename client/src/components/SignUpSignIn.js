4dadebdf8651d98cce8bcb0d8753d348a8aaf65b
rakinuddin <rakinuddin97@gmail.com>
2019-03-30 02:45:12 -0400

    sup

--- client/src/components/SignUpSignIn.js
+++ client/src/components/SignUpSignIn.js
@@ -12,7 +12,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 import { signupUser, loginUser } from '../actions/user';
 
-
 library.add([
   faUser,
   faEnvelope,
