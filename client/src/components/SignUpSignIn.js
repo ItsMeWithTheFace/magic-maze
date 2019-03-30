7d1d5026d96ce0eefbb22ffd1dd0602d885a2e64
rakinuddin <rakinuddin97@gmail.com>
2019-03-30 02:40:16 -0400

    added signup/signin pages

---
+++ client/src/components/SignUpSignIn.js
@@ -0,0 +1,190 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+  Card, CardHeader, CardBody, CardFooter,
+  Button, Form, FormGroup, Label, Input, Nav, NavItem, NavLink,
+} from 'reactstrap';
+import {
+  faUser, faEnvelope, faKey, faUserPlus, faSignInAlt,
+} from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
+import { signupUser, loginUser } from '../actions/user';
+
+
+library.add([
+  faUser,
+  faEnvelope,
+  faKey,
+  faUserPlus,
+  faSignInAlt,
+]);
+
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      page: 'register',
+    };
+  }
+
+  loginUser() {
+    const { firebase, loginUserProp, history } = this.props;
+    const { email, password } = this.state;
+    firebase
+      .doSignInWithEmailAndPassword(email, password)
+      .then(async () => {
+        const user = firebase.auth.currentUser;
+        const token = await user.getIdToken();
+
+        loginUserProp({
+          token,
+          username: user.displayName,
+          email: user.email,
+          uid: user.uid,
+        });
+
+        history.push('/');
+      });
+  }
+
+  signupUser() {
+    const { firebase, signupUserProp, history } = this.props;
+    const { email, password, username } = this.state;
+    firebase
+      .doCreateUserWithEmailAndPassword(email, password)
+      .then(async () => {
+        const user = firebase.auth.currentUser;
+        const token = await user.getIdToken();
+
+        if (user) {
+          await user.updateProfile({
+            displayName: username,
+          });
+
+          signupUserProp({
+            token,
+            username: user.displayName,
+            email: user.email,
+            uid: user.uid,
+          });
+
+          history.push('/');
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  render() {
+    const { history } = this.props;
+    const { page } = this.state;
+
+    const username = (
+      <FormGroup>
+        <Label for="usernameField">
+          <FontAwesomeIcon icon="user" />
+          &nbsp;Username
+        </Label>
+        <Input
+          onChange={e => this.setState({ username: e.target.value })}
+          type="text"
+          name="username"
+          id="usernameField"
+          placeholder="Enter a username"
+        />
+      </FormGroup>
+    );
+
+    const buttons = page === 'register' ? (
+      <Button color="primary" onClick={() => this.signupUser()}>Register</Button>
+    ) : (
+      <Button color="success" onClick={() => this.loginUser()}>Sign In</Button>
+    );
+
+    return (
+      <div>
+        <div className="cover">
+          <div className="container">
+            <Card>
+              <CardHeader>
+                <Nav pills>
+                  <NavItem>
+                    <NavLink href="#" active={page === 'register'} onClick={() => this.setState({ page: 'register' })}>
+                      <FontAwesomeIcon icon="user-plus" />
+                      &nbsp;Register
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="#" active={page === 'signin'} onClick={() => this.setState({ page: 'signin' })}>
+                      <FontAwesomeIcon icon="sign-in-alt" />
+                      &nbsp;Sign In
+                    </NavLink>
+                  </NavItem>
+                </Nav>
+              </CardHeader>
+              <CardBody>
+                <Form>
+                  { page === 'register' ? username : null }
+                  <FormGroup>
+                    <Label for="emailField">
+                      <FontAwesomeIcon icon="envelope" />
+                      &nbsp;Email
+                    </Label>
+                    <Input
+                      onChange={e => this.setState({ email: e.target.value })}
+                      type="email"
+                      name="email"
+                      id="emailField"
+                      placeholder="Enter your email address"
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="passwordField">
+                      <FontAwesomeIcon icon="key" />
+                      &nbsp;Password
+                    </Label>
+                    <Input
+                      onChange={e => this.setState({ password: e.target.value })}
+                      type="password"
+                      name="password"
+                      id="passwordField"
+                      placeholder="Enter your password"
+                    />
+                  </FormGroup>
+                </Form>
+              </CardBody>
+              <CardFooter style={{ textAlign: 'center' }}>
+                {buttons}
+                <Button color="secondary" className="ml-2" onClick={() => history.push('/')}>Back</Button>
+              </CardFooter>
+            </Card>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  firebase: state.firebaseReducer.firebaseInst,
+});
+
+const mapDispatchToProps = dispatch => ({
+  signupUserProp: user => dispatch(signupUser(user)),
+  loginUserProp: user => dispatch(loginUser(user)),
+});
+
+SignUp.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
+  firebase: PropTypes.shape({ auth: PropTypes.func.isRequired }).isRequired,
+  signupUserProp: PropTypes.func.isRequired,
+  loginUserProp: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
